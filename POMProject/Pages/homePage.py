e5803d8817d3d6869d57af068931c7653ab13e30
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-12-12 23:23:16 +0530

    first python

---
+++ POMProject/Pages/homePage.py
@@ -0,0 +1,16 @@
+from selenium.webdriver.common.by import By
+
+class HomePage():
+
+    def __int__(self,driver):
+        self.driver=driver
+
+        self.home_burger_menu_id='react-burger-menu-btn'
+        self.logout_link_id='logout_sidebar_link'
+
+    def click_on_home_burger_menu(self):
+        self.driver.find_element(By.ID, self.home_burger_menu_id).click()
+
+    def click_on_logout(self):
+        self.driver.find_element(By.ID, self.logout_link_id).click()
+
---
+++ POMProject/Tests/Login.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import unittest
+from POMProject.Pages.loginPage import LoginPage
+from POMProject.Pages.homePage import HomePage
+
+class LoginTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome()
+        #driver = webdriver.Chrome(executable_path='')
+        cls.driver = webdriver.Chrome()
+        cls.driver.implicitly_wait(40)
+        cls.driver.maximize_window()
+        time.sleep(5)
+
+    def test_login_valid(self):
+        self.driver.get("https://www.saucedemo.com/")
+        self.driver.find_element(By.ID, "user-name").send_keys('standard_user')
+        self.driver.find_element(By.ID, "password").send_keys('secret_sauce')
+        self.driver.find_element(By.ID, "login-button").click()
+        self.driver.find_element(By.ID, 'react-burger-menu-btn').click()
+        time.sleep(2)
+        self.driver.find_element(By.ID, 'logout_sidebar_link').click()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.close()
+        cls.driver.quit()
+        print("--------------------- Test completed -------------------------")
+
+
+if __name__ =='__main__':
+    unittest.main()
